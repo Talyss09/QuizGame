7d66260d50dd6b9cb2fc61ca533e6ed6c83d6c65
Talyss09 <talys.oliveira2018@hotmail.com>
2023-07-03 15:01:09 -0300

    added functions that make game works

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
-import { useContext, useEffect } from 'react'
-import { Body, Container, Content, Header, Score, Time } from './assets/styles/app'
+import { useContext, useEffect, useState } from 'react'
+import { Body, Container, Content, Header} from './assets/styles/app'
 import { QuestionCard } from './components/QuestionCard'
 import { StartPage } from './components/StartPage'
 import { QuizContext } from './context/quiz'
 import {FinalScore} from './components/FinalScore'
 export const App = () => {
   const [quizState, setQuizState] = useContext(QuizContext);
+  const [score, setScore] = useState(0)
+  const currentQuestion = quizState.Questions[quizState.currentQuestion]
+  
+  const optionClicked = (isCorrect) => {
+    if (isCorrect === currentQuestion.answer) {
+      setScore(score + 1)
+    } else {
+    }
+  }
+
+  useEffect(() => {
+    if(quizState.game === 'start'){
+      setScore(0)
+    }
+  }, [quizState])
 
   useEffect(() => {
     setQuizState({type: "ReOrder"})
   },[setQuizState])
+
   return (
     <Container>
         <Header>
-            <Score>
-              View Highscores
-            </Score>
-            <Time>Time: 38</Time>
+            {/* <Time>Time: 38</Time> */}
         </Header>
         <Body> 
           <Content>
             {quizState.game === "start" && <StartPage/>}
-            {quizState.game === "playing" && <QuestionCard/>}
-            {quizState.game === "end" && <FinalScore/>}
-            {/* <QuestionCard/> */} 
+            {quizState.game === "playing" && <QuestionCard Score={score} current={currentQuestion} onClick={(e) => {
+               setQuizState({type: 'ChangeQuestion'})
+               optionClicked(e.target.value)
+              }}/>}
+            {quizState.game === "end" && <FinalScore finalScore={score}/>}
           </Content>
         </Body>
     </Container>
